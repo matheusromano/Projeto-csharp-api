fc0ab4ba9debe2dcf4638d1cdb0dc8e869227f24
C1325543 Matheus Romano Braga <romanobraga.m@gmail.com>
2022-09-26 08:26:30 -0300

    projeto rpg api

--- rpgAPi/Program.cs
+++ rpgAPi/Program.cs
@@ -2,11 +2,9 @@ global using rpgAPi.Data;
 global using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=rpgDatabase.db";
+builder.Services.AddSqlite<DataContext>(connectionString);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
